8fe7f937c18576c41d857eb6f1753fdf0d7868c7
brodyee <erlandsonbrody@gmail.com>
2024-01-30 22:16:09 -0700

    done

--- src/helpers.h
+++ src/helpers.h
@@ -26,7 +26,8 @@ arma::vec getFit(const arma::mat& X, const arma::vec& y) {
 }
 
 arma::mat getStdErr(const arma::vec& residuals, const arma::mat& X) {
-  arma::mat sigHat = arma::var(residuals) * arma::inv(X.t() * X);
+  double varHat = ((X.n_rows - 1)/(X.n_rows - X.n_cols))*arma::var(residuals);
+  arma::mat sigHat = arma::sqrt(varHat * arma::inv(X.t() * X));
 
   return sigHat;
 }
@@ -37,12 +38,12 @@ arma::mat getConfInt(const arma::vec& beta, const arma::mat& stdErr,
   arma::mat confInt = arma::zeros(NUM_COEF, 2);
 
   for (int i = 0; i < NUM_COEF; i++) {
-    confInt(i, 0) = beta(i) 
+    confInt.at(i, 0) = beta[i] 
                     - R::qt(1 - alpha / 2, n - beta.n_elem, true, false) 
-                      * stdErr(i, i);
-    confInt(i, 1) = beta(i) 
+                      * stdErr.at(i, i);
+    confInt.at(i, 1) = beta[i]
                     + R::qt(1 - alpha / 2, n - beta.n_elem, true, false) 
-                      * stdErr(i, i);
+                      * stdErr.at(i, i);
   }
 
   return confInt;
